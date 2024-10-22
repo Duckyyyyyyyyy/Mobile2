f7bd9ecbf5677d3213182f683016317a7f4e2c52
Ducky <viet.tran.cit20@eiu.edu.vn>
2024-10-22 08:33:01 +0700

    add q3incometax

---
+++ Q3IncomeTax/styles.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+import {StyleSheet} from 'react-native';
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+  },
+  result: {
+    marginTop: 20,
+    fontSize: 16,
+  },
+});
